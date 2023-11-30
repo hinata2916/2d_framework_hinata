9548da9e671ef5e2a8b9aae6f5e7f4ea65bae4c9
S0517 <S0517@21S0517>
2023-11-30 09:49:04 +0900

    おはよう

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.util.Random;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+
+		static void kadaiB() {
+	        Random random = new Random();
+	        
+	        int randomNumber = random.nextInt(100) + 1;
+	       
+			System.out.println(randomNumber + "が出ました");
+		}
+		static void kadaiC() {
+			int a = 50;
+			int b = 75;
+			int c = a + b;
+			System.out.println(c);
+		}
+}
+
+
